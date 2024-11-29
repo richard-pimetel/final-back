3b4b72ead31f2b6cb3a20ffd69b0269e6f14bc36
RICHARD PIMENTEL <24122321@CFP127ALUNO.LOCAL>
2024-11-29 17:08:53 -0300

    continuacao

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -4,25 +4,29 @@ const lion_Curso = require('./cursos.js')
 
 
 const getListaDeCursos = function(){
-    let cursos = []
-    lion_Curso.cursos.forEach(function(curso){
-        cursos.push(curso)
-    })
-    return cursos
+    let listaCursos = [];
+
+    lion_Curso.cursos.forEach(function(curso) {
+        listaCursos.push(curso);
+    });
+
+    return listaCursos.length > 0 ? listaCursos : false;
 
 }
 //console.log(getListaDeCursos())
 
 const getListaDeAlunos = function(){
-    let alunos = []
-    lion_Aluno.alunos.forEach(function(aluno){
-        alunos.push(aluno)
-    })
-    return alunos
-}
+    let listaAlunos = [];
+
+    lion_Aluno.alunos.forEach(function(aluno) {
+        listaAlunos.push(aluno);
+    });
+
+    return listaAlunos.length > 0 ? listaAlunos : false;
+};
 //console.log(getListaDeAlunos())
 
-const getInformacoesDeAluno = function(matricula){
+const getMatriculaDeAluno = function(matricula){
     let inforAluno = null
     lion_Aluno.alunos.forEach(function(aluno){
     if(String(aluno.matricula) === matricula){
@@ -35,36 +39,84 @@ const getInformacoesDeAluno = function(matricula){
     })
     return inforAluno || false
 }
-//console.log(getInformacoesDeAluno('20151001002'))
+//console.log(getMatriculaDeAluno(''))
 
-const getListaDeTodosAlunosMatriculados = function(){
-    let listaDeAlunos = []
+const getDSouREDES = function(siglaCurso){
     
-
-
-
-
-
-
-}
-//console.log(getListaDeTodosAlunosMatriculados)
-
-const getListaDeTodosAlunosStatus = function(){
+    let listaAlunosCurso = [];
+    let cursoFormatado = String(siglaCurso).toUpperCase();
     
-}
-//console.log(getListaDeTodosAlunosStatus)
-
-const getListaDeAprovadosReprovadosExame = function(){
+    lion_Aluno.alunos.forEach(function(aluno) {
+        if (String(aluno.curso[0].sigla).toUpperCase() == cursoFormatado) {
+            listaAlunosCurso.push(aluno);
+        }
+    });
     
-}
-//console.log(getListaDeAprovadosReprovadosExame)
-
-const getListaDeAlunosCursoEspecificado = function(){
+     return listaAlunosCurso.length > 0 ? listaAlunosCurso : false
+};
     
-}
-//console.log(getListaDeAlunosCursoEspecificado)
+//console.log(getDSouREDES(''))
+
+const getStatus = function(filtroStatus){
+    let listaAlunos = [];
+    let statusFormatado = String(filtroStatus).toUpperCase();
+
+    lion_Aluno.alunos.forEach(function(aluno) {
+        if (String(aluno.status).toUpperCase() == statusFormatado) {
+            listaAlunos.push(aluno);
+        }
+    });
+
+    return listaAlunos.length > 0 ? listaAlunos : false
+};
+//console.log(getStatus(''))
 
 
+
+const getStatusExame = function(siglaCurso, statusCurso){
+
+        let cursoEscolhido = String(siglaCurso).toUpperCase();
+        let statusFormatado = String(statusCurso).toUpperCase();
+        let listaAlunosFiltrados = [];
+        
+        lion_Aluno.alunos.forEach(function(aluno) {
+            aluno.curso.forEach(function(curso) {
+                if (String(curso.sigla).toUpperCase() === cursoEscolhido) {
+                    curso.disciplinas.forEach(function(disciplina) {
+                        if (String(disciplina.status).toUpperCase() === statusFormatado) {
+                            listaAlunosFiltrados.push(aluno)
+                        }
+                    })
+                }
+            })
+        })
+    
+        return listaAlunosFiltrados.length > 0 ? listaAlunosFiltrados : false
+    }
+    
+console.log(getStatusExame("DS", "EXAME"))
+
+
+
+
+const getConclusao = function(siglaCurso, anoConclusao) {
+    let cursoEscolhido = String(siglaCurso).toUpperCase()
+    let anoEscolhido = String(anoConclusao).toUpperCase()
+    let listaAlunosFiltrados = []
+    let status = false
+    
+    lion_Aluno.alunos.forEach(function(aluno) {
+         aluno.curso.forEach(function(curso) {
+            if (String(curso.sigla).toUpperCase() === cursoEscolhido &&
+                String(curso.conclusao).toUpperCase() === anoEscolhido) {
+                listaAlunosFiltrados.push(aluno)
+                status = true
+            }
+        })
+    })
+    
+    return status ? listaAlunosFiltrados : status
+ }
 
 
 
